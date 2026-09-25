a1000ab69e304172a9ae0b94685e263c7e41908e
hendyngo-servers <ngogiaidy3@gmail.com>
2026-09-25 14:41:07 +0700

    Create audioEngine.ts

---
+++ src/utils/audioEngine.ts
@@ -0,0 +1,38 @@
+class AudioEngine {
+  private ctx: AudioContext | null = null;
+
+  private getContext(): AudioContext {
+    if (!this.ctx) {
+      this.ctx = new (window.AudioContext || (window as any).webkitAudioContext)();
+    }
+    return this.ctx;
+  }
+
+  playProceduralBeep(freq = 440, durationSec = 0.2) {
+    const ctx = this.getContext();
+    const osc = ctx.createOscillator();
+    const gain = ctx.createGain();
+
+    osc.type = 'sine';
+    osc.frequency.setValueAtTime(freq, ctx.currentTime);
+    gain.gain.setValueAtTime(0.1, ctx.currentTime);
+    gain.gain.exponentialRampToValueAtTime(0.0001, ctx.currentTime + durationSec);
+
+    osc.connect(gain);
+    gain.connect(ctx.destination);
+
+    osc.start();
+    osc.stop(ctx.currentTime + durationSec);
+  }
+
+  async speakClientSpeechFallback(text: string, lang = 'vi-VN') {
+    if ('speechSynthesis' in window) {
+      window.speechSynthesis.cancel();
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.lang = lang;
+      window.speechSynthesis.speak(utterance);
+    }
+  }
+}
+
+export const audioEngine = new AudioEngine();
